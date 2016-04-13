7e680e9fc4a5aa916d8133a0d7a76c2a1f2c866e
Administrator <admin@example.com>
2016-04-13 08:48:18 +0300

    Added AbstractEntity

---
+++ src/main/java/com/qoantum/phonebook/common/AbstractEntity.java
@@ -0,0 +1,7 @@
+package com.qoantum.phonebook.common;
+
+/**
+ * Created by qoantum on 4/12/16.
+ */
+public abstract class AbstractEntity {
+}
--- src/main/java/com/qoantum/phonebook/common/Entity.java
+++ src/main/java/com/qoantum/phonebook/common/Entity.java
@@ -20,4 +20,11 @@ public interface Entity extends Serializable, Cloneable, Comparable<Entity> {
      * @return RecordStatus entity status
      */
     RecordStatus getRecordStatus();
+
+    /**
+     * Set the status of a record
+     *
+     * @param recordStatus the status of the record
+     */
+    void setRecordStatus(RecordStatus recordStatus);
 }
