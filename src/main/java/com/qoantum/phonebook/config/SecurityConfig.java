3a50f3768acf51644e760bd704946070811e7e81
Administrator <admin@example.com>
2016-04-28 07:32:48 +0300

    Security Added

---
+++ src/main/java/com/qoantum/phonebook/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.qoantum.phonebook.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Security configuration
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //super.configure(http);
+        http
+                .httpBasic().and()
+                .authorizeRequests()
+                    .antMatchers(HttpMethod.POST, "/qoantum/phonebook/users", "/qoantum/phonebook/roles", "/qoantum/phonebook/groups").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.PUT, "/qoantum/phonebook/users", "/qoantum/phonebook/roles","/qoantum/phonebook/groups").hasRole("ADMIN")
+                    .antMatchers(HttpMethod.PATCH, "/qoantum/phonebook/users", "/qoantum/phonebook/roles","/qoantum/phonebook/groups").hasRole("ADMIN")
+                    .antMatchers("/users*", "/roles*", "/groups*").hasRole("USER").and()
+                .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //super.configure(auth);
+        auth
+                .inMemoryAuthentication()
+                .withUser("bello").password("bello").roles("USER").and()
+                .withUser("admin").password("admin").roles("USER", "ADMIN");
+    }
+}
